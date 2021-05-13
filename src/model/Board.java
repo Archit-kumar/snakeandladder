204bad57a0a9fbd62d87c9b294fd9b27334f1d71
Archit Kumar <architkumar.sh@gmail.com>
2021-05-13 17:56:03 +0530

    added Board model

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,7 +1,61 @@
 package model;
 
+import java.util.HashMap;
+import java.util.List;
+
 class Board{
 
     private int size;
 
+    private List<Snake> snakes;
+    
+    private List<Ladder> ladders;
+    
+    private HashMap<String, Integer> playesrLocation;
+    
+    
+
+	public Board(int size, List<Snake> snakes, List<Ladder> ladders, HashMap<String, Integer> playesrLocation) {
+		super();
+		this.size = size;
+		this.snakes = snakes;
+		this.ladders = ladders;
+		this.playesrLocation = playesrLocation;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public List<Snake> getSnakes() {
+		return snakes;
+	}
+
+	public void setSnakes(List<Snake> snakes) {
+		this.snakes = snakes;
+	}
+
+	public List<Ladder> getLadders() {
+		return ladders;
+	}
+
+	public void setLadders(List<Ladder> ladders) {
+		this.ladders = ladders;
+	}
+
+	public HashMap<String, Integer> getPlayesrLocation() {
+		return playesrLocation;
+	}
+
+	public void setPlayesrLocation(HashMap<String, Integer> playesrLocation) {
+		this.playesrLocation = playesrLocation;
+	}
+    
+    
+    
+    
 }
